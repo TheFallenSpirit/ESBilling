4923a26847ac229a16a7e50048526ee043d96299
Liam Labell <thefallenspirit@outlook.com>
2024-09-20 02:55:45 -0700

    Add payment fail event handler

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
     feedbackMessage: 'If you have any feedback for us, please let us know in [ES Community](<https://discord.gg/rhe42NjaUr>).',
-    manageMessage: '<:es_info:1253600321557626890> You can manage this subscription by selecting it from the selector after using </home:1275068280955998299>.'
+    manageMessage: '<:es_info:1253600321557626890> You can manage this subscription in the </home:1275068280955998299> panel.'
 };
---
+++ src/events/paymentFail.ts
@@ -0,0 +1,20 @@
+import config from '../config';
+import { getForName } from '../helpers';
+import { getSubscription, redis } from '../store';
+import { CustomData, WebhookPayload } from '../types';
+
+export default async (body: WebhookPayload<CustomData>) => {
+    let subscription = await getSubscription(body.meta.custom_data!.subscriber_id, body.data.attributes.subscription_id!.toString());
+    if (!subscription) return;
+
+    const forName = await getForName(subscription);
+
+    const lines = [
+        `Hey there! Just wanted to let you know that we were unable to charge your payment method for your`,
+        ` ${subscription.planName} subscription${forName ? ` for ${forName}` : ''}.\n${config.manageMessage}`,
+    ];
+
+    await redis.lpush(`es_queue:${process.env.BOT_ID}:billing`, JSON.stringify({
+        force: true, user: subscription.subscriberId, content: { content: lines.join('') }
+    }));
+};
