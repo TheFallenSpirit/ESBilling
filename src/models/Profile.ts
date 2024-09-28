330e10fa500037395746d3ae56ffe55b49c3593e
Liam Labell <thefallenspirit@outlook.com>
2024-09-28 10:19:20 -0700

    [CRIT] Proper data types for map objects

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -2,15 +2,41 @@
 
 import { model, Schema } from 'mongoose';
 
+export interface BindI {
+	channel: string;
+	roles?: string[];
+}
+
+export interface ChastityI {
+	role?: string;
+	channels?: string[];
+}
+
 export interface ProfileI {
     user: string;
     premiumTier: number;
+    binds: Map<string, BindI>;
+    chastity: Map<string, ChastityI>;
 };
+
+// prettier-ignore
+const bindSchema = new Schema<BindI>({
+	channel: { required: true, type: String },
+	roles: { required: false, type: [String] }
+}, { _id: false, versionKey: false });
+
+// prettier-ignore
+const chastitySchema = new Schema<ChastityI>({
+	role: { required: false, type: String },
+	channels: { required: false, type: [String] }
+}, { _id: false, versionKey: false });
 
 // prettier-ignore
 const profileSchema = new Schema<ProfileI>({
     user: { required: true, type: String },
-    premiumTier: { required: true, type: Number, default: 0 }
+    premiumTier: { required: true, type: Number, default: 0 },
+    binds: { required: true, type: Map, of: bindSchema, default: new Map() },
+    chastity: { required: true, type: Map, of: chastitySchema, default: new Map() }
 }, { _id: false, versionKey: false, timestamps: true });
 
 export default model('profiles', profileSchema);
